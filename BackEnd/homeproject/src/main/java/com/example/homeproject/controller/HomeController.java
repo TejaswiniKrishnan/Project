341281ddabb3615aa283b076e24ff8914474bf52
Tejaswini <124782630+TejaswiniKrishnan@users.noreply.github.com>
2024-02-21 10:37:17 +0530

    Update HomeController.java

--- BackEnd/homeproject/src/main/java/com/example/homeproject/controller/HomeController.java
+++ BackEnd/homeproject/src/main/java/com/example/homeproject/controller/HomeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +48,15 @@ public class HomeController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PutMapping("/home/{id}")
+    public ResponseEntity<Home> update(@PathVariable int id, @RequestBody Home home) {
+        return new ResponseEntity<>(homeService.update(id, home), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/home/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable int id) {
+        return new ResponseEntity<>(homeService.delete(id), HttpStatus.OK);
     }
 }
